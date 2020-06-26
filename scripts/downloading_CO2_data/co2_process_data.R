76b4fb7e8860f104dd4477829e22226f30bdf559
Gonzalez <GonzalezB2@SI.EDU>
2020-06-26 11:11:33 -0400

    adding script changes

--- scripts/downloading_CO2_data/co2_process_data.R
+++ scripts/downloading_CO2_data/co2_process_data.R
@@ -22,10 +22,8 @@ ice_core <- read_excel(paste0(getwd(), path, "NOAA_law2006_ice_core_data.xls"),s
 
 #### Process data #### -------
 # average the ppm for all twelve values in `year` val for Moana Loa 
-avg_ml<- moana %>% group_by(year) %>% summarise_at(vars(value), list(mean))
-
-# grab only useful vals from ml
-avg_ml<- avg_ml[-c(1,2),]
+# filter -999 values NA values
+avg_ml<- moana %>% filter(value != -999.99) %>% group_by(year) %>% summarise_at(vars(value), list(mean))
 
 # ice_core data - grab vals from 1901 - 1975
 core <-ice_core[-c(1,2),]  # drop extranneous info from data
@@ -35,11 +33,11 @@ core<-core[-1,] #drop xtra col
 core<-core %>% select('Year AD', 'CO2 Spline (ppm)') # select only interested in vals
 core<-sapply(core, as.numeric) %>% as.data.frame()# convert to numeric
 
-core<- dplyr::filter(core, `Year AD` > 1900 & `Year AD` < 1976) # everything above 1974 to combine with ML data
+core<- dplyr::filter(core, `Year AD` > 1900 & `Year AD` < 1974) # everything above 1974 to combine with ML data
 core<-rename(core, value ='CO2 Spline (ppm)', year = 'Year AD') # rename to match ML data
 
 #### Combine Ice Core data and Moana Loa Data #### ------
 c02data<- rbind(core, avg_ml) %>% rename(c02_ppm=value)
 
 #### write CSVs make sure you always have argument row.names = FALSE ####
-write.csv(c02data, paste0(getwd(), "/Other_environmental_data/c02_MOANA_NOAA_combined.csv"), row.names =F)
+#write.csv(c02data, paste0(getwd(), "/Other_environmental_data/c02_data/c02_MOANA_NOAA_combined.csv"), row.names =F)

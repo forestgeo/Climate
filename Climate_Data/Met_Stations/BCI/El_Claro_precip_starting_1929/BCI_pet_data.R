22329e16df7ee7ca02e67da5265c6134be35fed9
Gonzalez <GonzalezB2@SI.EDU>
2020-07-13 09:51:47 -0400

    updating BCI file

--- Climate_Data/Met_Stations/BCI/El_Claro_precip_starting_1929/BCI_pet_data.R
+++ Climate_Data/Met_Stations/BCI/El_Claro_precip_starting_1929/BCI_pet_data.R
@@ -11,6 +11,7 @@ library(anytime)
 library(dplyr)
 library(viridis)
 library(lubridate)
+library(padr)
 ##########
 
 # Clean environment ####
@@ -30,6 +31,9 @@ seventies_rain$Date <- as.Date(seventies_rain$Date, "%d/%m/%Y")
 seventies_PRE <- seventies_rain %>% group_by(Date=floor_date(Date, "month")) %>% summarize(climvar.val=sum(avg_rain))
 pre_1900s <- rain_1900s %>% group_by(Date=floor_date(Date, "month")) %>% summarize(climvar.val=sum(`rain (mm)`))
 
+## filter in pre 1900s until date that seventies pre doesnt exist
+pre_1900s<- pre_1900s %>% filter(Date < as.Date("1971-04-01"))
+
 # bind these puppies, add relevent cols, and write.csv
 pre_BCI <- rbind(pre_1900s, seventies_PRE)
 pre_BCI$sites.sitename <- rep("Barro Colorado Island", nrow(pre_BCI))
@@ -39,8 +43,12 @@ pre_BCI$climvar.class <- rep("pre", nrow(pre_BCI))
 # rearrange cols to match long format
 pre_BCI<- pre_BCI[c(3,1,2,5,4)]
 
+# fill in missing dates 
+pad(pre_BCI, interval = "month")
+pre_BCI %>% complete(Date = seq.Date(min(Date), max(Date), by="month"))
+
 ## write.csv for PRE
-#write.csv(pre_BCI, paste0(getwd(), "/Climate_Data/Met_Stations/BCI/pre_BCI.csv"), row.names = F)
+#write.csv(pre_BCI, paste0(getwd(), "/Climate_Data/Met_Stations/BCI/El_Claro_precip_starting_1929/pre_BCI.csv"), row.names = F)
 
 ## FOR WET FORMATTING ----
 
@@ -49,6 +57,9 @@ pre_BCI<- pre_BCI[c(3,1,2,5,4)]
 
 wet_1900s<- rain_1900s %>% group_by(Date=floor_date(Date, "month")) %>% filter(`rain (mm)`>.1) %>% tally()
 wet_1970s<- seventies_rain %>% group_by(Date=floor_date(Date, "month")) %>% filter(avg_rain>.1) %>% tally()
+
+## filter in pre 1900s until date that seventies pre doesnt exist
+wet_1900s<- wet_1900s %>% filter(Date < as.Date("1971-04-01"))
 
 # bind these puppies, add relevent cols, and write.csv
 wet_BCI <- rbind(wet_1900s, wet_1970s)
@@ -61,4 +72,4 @@ wet_BCI<- wet_BCI %>% rename(climvar.val= n)
 wet_BCI<- wet_BCI[c(3,1,2,5,4)]
 
 ## write.csv for PRE
-write.csv(wet_BCI, paste0(getwd(), "/Climate_Data/Met_Stations/BCI/wet_BCI.csv"), row.names = F)
+#write.csv(wet_BCI, paste0(getwd(), "/Climate_Data/Met_Stations/BCI/El_Claro_precip_starting_1929/wet_BCI.csv"), row.names = F)

45d7d85a423e7d7f2a201672838e9135bffa6c94
Gonzalez <GonzalezB2@SI.EDU>
2020-09-09 14:37:05 -0400

    adding updated script

--- Other_environmental_data/so2_nox_data/forest_geo_nos_so2.R
+++ Other_environmental_data/so2_nox_data/forest_geo_nos_so2.R
@@ -21,81 +21,74 @@ ForestGEO_sites <- ForestGEO_sites[!is.na(ForestGEO_sites$Lat),]
 points <- ForestGEO_sites
 coordinates(points)<-c("lon", "lat") #long and lat
 
-#create a spatial points dataframe
+# create a spatial points dataframe
 proj<-CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
 proj4string(points)<-proj
 
-# list files with the .nc extension
-ncfilenames <- list.files(path = paste0('C:/Users/GonzalezB2/Desktop/Smithsonian/CEDS/intermediate-output/gridded-emissions/SO2'), pattern = "\\.nc") # replace with your local path name
+#### list files with the .nc extension
+### Make sure the change this file extension if loooking at another folder
 
-setwd("C:/Users/GonzalezB2/Desktop/Smithsonian/CEDS/intermediate-output/gridded-emissions/SO2")
+## SO2
+ncfilenames <- list.files(path = paste0('C:/Users/GonzalezB2/Desktop/Smithsonian/CEDS/intermediate-output/gridded-emissions/SO2'), pattern = "\\.nc")
 
-# ncfilenames will have to change
-#tt<-brick(paste0(getwd(),"/", ncfilenames[1]),)
+## NOX
+ncfilenames <- list.files(path = paste0('C:/Users/GonzalezB2/Desktop/Smithsonian/CEDS/intermediate-output/gridded-emissions/NOx'), pattern = "\\.nc")
+
+### Change this too to reflect the dir we're interested in
+setwd("C:/Users/GonzalezB2/Desktop/Smithsonian/CEDS/intermediate-output/gridded-emissions/NOx")
+
+### R hack-- function converting factor to to numeric
+funny <- function(poopy){
+    as.numeric(as.character(poopy))
+}
+
+# emissions sectors
 vars<- c("AGR", "ENE", "IND", "TRA", "SLV", "WST", "SHP", "RCO")
 # for all of these varnames for that year make a df and bind to the other year (if not all 0 values)
-testy<- raster(paste0(getwd(),"/", ncfilenames[1]), varname="RCO")
-t<- raster::extract(testy, points)
 
-for(file in ncfilenames){
+for(file in ncfilenames) {
     print(file)
+    for(v in vars) {
+        print(paste0("working on var ", v))
+        ## extract emission values from raster object at forestgeo points
+        r<-brick(paste0(getwd(),"/", file),varname=v)  # use brick to create a raster obj and NOT raster because extracts all yrs info
+        x<- raster::extract(r, points)
 
-    ## extract emission values from raster object at forestgeo points
-    #r <- brick(paste0(getwd(),"/", file))  # create a raster brick object from .nc file
-    r<- raster(paste0(getwd(),"/", file), varname="RCO")
-    x <- raster::extract(r,points) # extract values from raster-brick at pts
+        ### add relevent columns
+        x <- cbind(sites.sitename = gsub(" ", "_", points@data$Site.name), x) # add sites.sitename
+        x <- cbind(emission_source = rep(r@title, nrow(x)), x)
+        x <- cbind(emission_units = rep(r@data@unit, nrow(x)), x)
+        x <- cbind(emission_type = rep(stringr::str_sub(file, start=6, end=8), nrow(x)),x)
 
-    ### add relevent columns
-    x <- cbind(sites.sitename = gsub(" ", "_", points@data$Site.name), x) # add sites.sitename
-    x <- cbind(emission_source = rep(r@title, nrow(x)), x)
-    x <- cbind(emission_units = rep(r@data@unit, nrow(x)), x)
-    x <- cbind(emission_type = rep(stringr::str_sub(file, start=6, end=8), nrow(x)),x)
-   # x <- cbind(points,x) # grab the lat longs and rest of file info if wanted
-   # x <- cbind(year = rep(as.integer(stringr::str_sub(gsub("X","", r@data@names[1]), start=1, end=4)), nrow(x)),x)
+        ### R hack to grab RowSums
+        x<-as.data.frame(x)
+        x[,5:16]<- sapply(x[,5:16], funny) # converting to numeric using R hack function
+        x$year_sum<- rowSums(x[,5:16]) #base package sums for rows
+        names(x)[names(x) == "year_sum"] <- paste0(substr(r@z$Date[1], 1,4)) # rename year_sum column to yr
 
-    if(file[1]==ncfilenames[1]){
-        all_DEP <- x
+        # delete all `X`` columns
+        x <- x[,grepl("X", colnames(x))==FALSE]
 
-    }
+        ### Add to master dataset & merge
+        if(file[1]==ncfilenames[1] & v[1]==vars[1]){ # if first file and var
+            all_DEP <- x
+        }
+        else{
 
-    else {
-        print("merging years")
-        all_DEP <- merge(all_DEP, x, by = c("sites.sitename", "emission_source", "emission_type", "emission_units"), all = T)
-    }
-}
-
-ncfilenames_so2 <- list.files(path = paste0('C:/Users/GonzalezB2/Desktop/Smithsonian/CEDS/intermediate-output/gridded-emissions/SO2'), pattern = "\\.nc") # replace with your local path name
-test <- list.files(path = paste0('C:/Users/GonzalezB2/Desktop/Smithsonian/CEDS/intermediate-output/gridded-emissions/test'), pattern = "\\.nc") # replace with your local path name
-setwd("C:/Users/GonzalezB2/Desktop/Smithsonian/CEDS/intermediate-output/gridded-emissions/")
-
-emission_calc <- function(spa_file){  # the arguement is going to be the filenames(nc)
-counter=0
-for(file in spa_file){
-counter= counter+1
-   r <- brick(paste0(getwd(),"/",stringr::str_sub(file[1], start=6, end=8),"/", file))  # create a raster brick object from .nc file
-   print(file)
-   x <- raster::extract(r,points) # extract raster-brick at pts
-
-   ### add relevent columns
-   x <- cbind(sites.sitename = gsub(" ", "_", points@data$Site.name), x) # add sites.sitename
-   x <- cbind(emission_source = rep(r@title, nrow(x)), x)
-   x <- cbind(emission_units = rep(r@data@unit, nrow(x)), x)
-   x <- cbind(emission_type = rep(stringr::str_sub(file, start=6, end=8), nrow(x)),x)
-   # x <- cbind(year = rep(as.integer(stringr::str_sub(gsub("X","", r@data@names[1]), start=1, end=4)), nrow(x)),x)
-
-   if(file[1]==ncfilenames[1]){
-       print("first df")
-       all_DEP <- x
-   }
-
-   else {
-       print(paste0("merging years",counter ))
-       all_DEP <- merge(all_DEP, x, by = c("sites.sitename", "emission_source", "emission_type", "emission_units"), all = T)
+            all_DEP <- merge(all_DEP, x,by =  , all = T) # merge by everything except for the year value -
         }
     }
-# here make sure we write the relevant
-print("out of forloop and writing the emissions file")
-write.csv(all_DEP,paste0(getwd(), "/","emissions_",stringr::str_sub(file[1], start=6, end=8), ".csv"))
 }
 
-filter_all(all_DEP, any_vars(. != 0)) %>% View()
+### Change to Tidy format // gather values //filter extra NAs from merge process
+DEP_clean <-  pivot_longer(all_DEP,cols= starts_with("1"),
+                        names_to= "year",values_to = "value") %>%
+                        filter(!is.na(value))
+
+### Group and sum deposition values
+DEP_clean$year<- str_sub(DEP_clean$year, 1,4) # clean year value to group later
+DEP_clean<- DEP_clean %>% group_by(year, sites.sitename, emission_units, emission_type) %>%
+                         summarise(value = sum(value))# sum across all industries
+
+### Okay great! now let's CSV this puppy
+#write.csv(DEP_clean, "C:/Users/GonzalezB2/Desktop/Smithsonian/Climate/Other_environmental_data/nox_emissions_wout_2000s.csv", row.names=FALSE)

d773112f50cf1064732e1d872997075bfbef7f95
Valentine Herrmann <herrmannv@si.edu>
2022-10-14 15:13:47 -0400

    calculate pet_sum

--- Climate_Data/CRU/scripts/Calculate_PET_sum.R
+++ Climate_Data/CRU/scripts/Calculate_PET_sum.R
@@ -37,7 +37,7 @@ pet <- read.csv("Climate_Data/CRU/CRU_v4_06/pet.1901-2021-ForestGEO_sites.csv")
 
 # multiply each value by the number of day of that month
 nb.days.in.month <- days_in_month(as.Date(paste(pet$Yr, pet$Mo, 01, sep = "-"), format = "%Y-%m-%d"))
-pet$value <- pet$value * nb.days.in.month
+pet$Value <- pet$Value * nb.days.in.month
 
 #save ####
 write.csv(pet, "Climate_Data/CRU/CRU_v4_06/pet_sum.1901-2021-ForestGEO_sites.csv", row.names = F)
